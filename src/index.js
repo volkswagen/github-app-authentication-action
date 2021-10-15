5a6c02a85383cb6a63d7bb70ab6b8ace70238d04
Lars Gohr <elgohr@oss.volkswagen.com>
2021-10-15 15:00:55 +0200

    :recycle: Have an async wrapper in main

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@
 const { getInput, setFailed, setOutput } = require('@actions/core');
 const { createAppAuth } = require('@octokit/auth-app');
 const { authenticate } = require('authenticate');
-await authenticate(createAppAuth, getInput, setOutput, setFailed);
+(async () => {
+    await authenticate(createAppAuth, getInput, setOutput, setFailed);
+})();
