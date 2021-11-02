2c2f35e0c5f4b33fc8959ca05139f4c674eeddcd
Lars Gohr <elgohr@oss.volkswagen.com>
2021-11-02 09:14:46 +0100

    :recycle: Add linter for cleaner code

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 "use strict";
-const { getInput, setFailed, setOutput } = require('@actions/core');
-const { createAppAuth } = require('@octokit/auth-app');
+const { getInput, setFailed, setOutput } = require("@actions/core");
+const { createAppAuth } = require("@octokit/auth-app");
 
 async function authenticate(authFunc, getInput, setOutput, setFailed) {
     try {
-        const installationId = getInput('installationId', { required: true });
+        const installationId = getInput("installationId", { required: true });
         const auth = authFunc({
-            appId: getInput('appId', { required: true }),
-            privateKey: getInput('privateKey', { required: true }),
-            clientId: getInput('clientId', { required: true }),
-            clientSecret: getInput('clientSecret', { required: true }),
+            appId: getInput("appId", { required: true }),
+            privateKey: getInput("privateKey", { required: true }),
+            clientId: getInput("clientId", { required: true }),
+            clientSecret: getInput("clientSecret", { required: true }),
         });
         const installationAuthentication = await auth({
             type: "installation",
